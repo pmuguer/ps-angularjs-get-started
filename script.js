8718efb674396659574edf8019f6bd4d96182c86
Pablo Muguerza <pmuguer@gmail.com>
2019-03-02 17:26:29 -0300

    Agregado ng-click para que el input del usuario se pueda usar para realizar la búsqueda

--- script.js
+++ script.js
@@ -9,14 +9,28 @@
 
     var MainController = function ($scope, $http) {
 
+        // Inicializo commentId para que al ingresar se muestre el primer comentario
         $scope.commentId = "1";
+
+        // Función asociada via ng-click al submit
+        // Notar que no necesito el parámetro, porque ya está en el $scope (commentId)
+        $scope.search = function() {
+            $http.get("https://jsonplaceholder.typicode.com/comments/" + $scope.commentId)
+            .then(onHTTPRequestComplete, onHTTPRequestError);
+        }
 
         var onHTTPRequestComplete = function(response) {
             $scope.comment = response.data;
+            // De forma análoga al caso de error, si el request fue exitoso tengo
+            // que resetear error
+            $scope.error = "";
         };
 
         var onHTTPRequestError = function(reason) {
             $scope.error = "Error accediendo al recurso REST";
+            // Si no pude obtener datos, dejo en blanco para que no se muestren los
+            // obtenidos en el último request válido
+            $scope.comment = "";
         };
     
         $http.get("https://jsonplaceholder.typicode.com/comments/" + $scope.commentId)
