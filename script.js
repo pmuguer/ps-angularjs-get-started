bb942173309a314b5d72778fe5e88b676e4952c0
Pablo Muguerza <pmuguer@gmail.com>
2019-03-02 12:53:54 -0300

    Arreglado código roto por el uso de un controller definido en el scope global
    A partir de AngularJS 1.2.25, el framework no busca controllers en el scope global.
    Para arreglar esto se definió un módulo en el que se registra el controller

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-// Code goes here
+(function () {
 
-var MainController = function($scope) {
+    // Defino el módulo getStartedExample1
+    // 
+    // Es importante incluir el segundo parámetro (empty array = []),
+    // porque de lo contrario no se estaría definiendo el módulo, sino
+    // que sólo se estaría incluyendo una referencia
+    var app = angular.module("getStartedExample1", []);
 
-    $scope.message = "Hello AngularJS!";
+    var MainController = function ($scope) {
 
-};
+        $scope.message = "Hello AngularJS!";
+
+    };
+
+    // Registro el controller en el módulo recién creado
+    app.controller("MainController", MainController);
+
+}());
