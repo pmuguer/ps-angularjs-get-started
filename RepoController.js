32eb422dae515c4b7067c60a9b230f9474ab274c
Pablo Muguerza <pmuguer@gmail.com>
2019-03-04 16:35:26 -0300

    Corregido RepoController; si las llamadas a los servicios están antes de la definición de los callbacks, no funciona el controller

--- RepoController.js
+++ RepoController.js
@@ -1,34 +1,44 @@
 (function(){
     
-    var app = angular.module("getStartedExercise1");
+    var app = angular.module("getStartedExample1");
 
     var RepoController = function($scope, github, $routeParams) {
 
-        $scope.username = $routeParams.username
-        $scope.reponame = $routeParams.reponame
+        $scope.username = $routeParams.username;
+        $scope.reponame = $routeParams.reponame;
+        $scope.collaborators = {};
+        $scope.error = "";
         
-        github.getRepo($scope.username, $scope.reponame)
-            .then(setRepoData(repo))
-
-        var setRepoData = function (repo) {
+        var setRepoData = function(repo) {
             $scope.repo = repo;
         };
 
-        // Para obtener los collaborators se requiere autenticación,
-        // en lugar de eso obtengo los followers
-        github.getFollowers($scope.username)
-            .then(setFollowersData);
+        var onHTTPRequestError = function(reason) {
+            $scope.error = "Error accediendo al recurso REST";
+            // Si no pude obtener datos, dejo en blanco para que no se muestren los
+            // obtenidos en el último request válido
+            $scope.user = "";
+        };
 
         var setFollowersData = function(followers) {
+            $scope.followers = followers;
             Object.keys(followers).forEach(function(follower) {
                 github.getUser(follower)
-                    .then(appendFollower)
+                    .then(appendFollower, onHTTPRequestError)
             });
         };
 
         var appendFollower = function(follower) {
             $scope.collaborators[follower.login] = follower;
         };
+
+        github.getRepo($scope.username, $scope.reponame)
+            .then(setRepoData, onHTTPRequestError);
+
+        // Para obtener los collaborators se requiere autenticación,
+        // en lugar de eso obtengo los followers
+        github.getFollowers($scope.username)
+            .then(setFollowersData, onHTTPRequestError);
 
     };
 
